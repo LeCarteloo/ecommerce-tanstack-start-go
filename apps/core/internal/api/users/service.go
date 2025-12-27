0aaf28886e5394a21dbfeb49ad5e7878105d66e9
Filip Papiernik <filippapiernik1999@gmail.com>
2025-12-27 19:45:00 +0100

    feat/EMBE-4: Add error handling

--- apps/core/internal/api/users/service.go
+++ apps/core/internal/api/users/service.go
@@ -2,23 +2,35 @@ package users
 
 import (
 	"context"
+	"errors"
 
 	repo "github.com/LeCarteloo/ecommerce-tanstack-start-go/internal/adapters/postgresql/sqlc"
+	"github.com/LeCarteloo/ecommerce-tanstack-start-go/internal/apperrors"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
-func NewUserService(r repo.Querier) *Service {
+func NewService(r repo.Querier) *Service {
 	return &Service{
 		repo: r,
 	}
 }
 
-func (s *Service) GetUserByID(ctx context.Context, userID pgtype.UUID) (repo.GetUserByIDRow, error) {
-	return repo.GetUserByIDRow{}, nil
+func (s *Service) GetUserByID(ctx context.Context, userId pgtype.UUID) (repo.GetUserByIDRow, error) {
+	user, err := s.repo.GetUserByID(ctx, userId)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return repo.GetUserByIDRow{}, apperrors.ErrUserNotFound
+		}
+
+		return repo.GetUserByIDRow{}, err
+	}
+
+	return user, nil
 }
 
 type UserService interface {
-	GetUserByID(ctx context.Context, userID pgtype.UUID) (repo.GetUserByIDRow, error)
+	GetUserByID(ctx context.Context, userId pgtype.UUID) (repo.GetUserByIDRow, error)
 }
 
 type Service struct {
--- apps/core/internal/api/users/service_test.go
+++ apps/core/internal/api/users/service_test.go
@@ -2,23 +2,25 @@ package users
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	repo "github.com/LeCarteloo/ecommerce-tanstack-start-go/internal/adapters/postgresql/sqlc"
 	"github.com/LeCarteloo/ecommerce-tanstack-start-go/internal/api/users/mocks"
+	"github.com/LeCarteloo/ecommerce-tanstack-start-go/internal/apperrors"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetUserByID(t *testing.T) {
 	ctx := context.Background()
-	mockRepo := new(mocks.MockQuerier)
 	userID := pgtype.UUID{
 		Bytes: [16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
 	}
 
 	t.Run("should return user when user exists", func(t *testing.T) {
-		userService := NewUserService(mockRepo)
+		mockRepo := new(mocks.MockQuerier)
+		userService := NewService(mockRepo)
 		expectedUser := repo.GetUserByIDRow{
 			ID:        userID,
 			Username:  "user",
@@ -35,5 +37,16 @@ func TestGetUserByID(t *testing.T) {
 		if assert.NotNil(t, user) {
 			assert.EqualValues(t, expectedUser, user)
 		}
+	})
+
+	t.Run("should return not found error if not found", func(t *testing.T) {
+		mockRepo := new(mocks.MockQuerier)
+		userService := NewService(mockRepo)
+
+		mockRepo.On("GetUserByID", ctx, userID).Return(repo.GetUserByIDRow{}, errors.New("not found"))
+
+		user, err := userService.GetUserByID(ctx, userID)
+		assert.Error(t, err, apperrors.ErrUserNotFound)
+		assert.EqualValues(t, repo.GetUserByIDRow{}, user)
 	})
 }
